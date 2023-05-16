2a536f677000ec0f89712e32ec26087b7d1d62b6
ambikaawari <132896716+ambikaawari@users.noreply.github.com>
2023-05-16 20:04:01 +0530

    Delete backend.tf

--- backend.tf
+++
@@ -1,8 +0,0 @@
-# terraform {
-#   backend "azurerm" {
-#     resource_group_name  = "storage001"
-#     storage_account_name = "stg22111"
-#     container_name       = "sst"
-#     key                  = "terraform.tfstate"
-#   }
-# }
