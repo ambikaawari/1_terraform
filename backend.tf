80321a4cfd6e832f0189490d078d7af14b10c828
ambikaawari <132896716+ambikaawari@users.noreply.github.com>
2023-05-16 19:53:12 +0530

    Create backend.tf

---
+++ backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "azurerm" {
+    resource_group_name  = "storage001"
+    storage_account_name = "stg22111"
+    container_name       = "sst"
+    key                  = "terraform.tfstate"
+  }
+}
